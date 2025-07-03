b20f6b048cff82729b1fde3de6330583be91d09b
anjana bharamnaikar <anjanabharamnaikar@anjanas-MacBook-Air.local>
2025-07-03 10:07:45 +0530

    etl pipeline

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+
+LOG_FILE_PATH = logs_path
+import logging
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
